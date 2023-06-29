12c9c53fa5de79910478692e0fcdc48bb4477cfc
Arthur Ilyashenko <ilyashenko.arthur@gmail.com>
2023-06-29 12:18:40 +0300

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -12,10 +12,6 @@ use Dotenv;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
